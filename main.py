2aaf7a8e75a97f58deaca61a10850aa5f8034762
SebastianAlberto1 <albertoandino250@gmail.com>
2023-09-29 18:07:05 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,20 @@ import sys
 import os
 from pygame.locals import *
 
-SCREEN_SIZE = (1000,800)
-FLOOR_POSITION = 700
+SCREEN_SIZE = (1000,600)
+FLOOR_POSITION = 550
 
+keypressed = {'a':False, 'd':False}
+class Game():
+    def __init__(self,imagePath,screen) -> None:
+        self.img = pygame.image.load(os.path.join("./", imagePath))
+        self.screen = screen
+    pass
+
+    def draw_background(self):
+        self.screen.blit(self.img,(0,0))
+        pass
+        
 
 class Entity () :
     def __init__(self, imagePath, size, screen) -> None:
@@ -16,7 +27,10 @@ class Entity () :
         self.pos = [SCREEN_SIZE[0]/2, 0]
         self.velocity = [0,0]
         pass
-    
+    def changePosition(self,coordinates):
+        self.pos[0] = coordinates [0]
+        self.pos[1] = coordinates [1]
+
     def draw(self):
         self.screen.blit(self.img, (self.pos[0],self.pos[1]))
         pass
@@ -26,57 +40,48 @@ class Entity () :
         self.pos[1] = self.pos[1] + self.velocity[1]
 
         if(self.pos[1] < FLOOR_POSITION):
-            ball.velocity[1] = ball.velocity[1] + 0.01
+            self.velocity[1] = self.velocity[1] + 0.01
         else:
-            ball.velocity[1] = 0
+            self.velocity[1] = 0
+        if (keypressed ['d']):
+            self.velocity [0] = 1
+        else:
+            self.velocity[0] = 0
+            pass 
+        
+        
 
         self.draw()
         pass
-
-
-
-
+    def jump(self):
+        self.velocity[1] = -4
 
 pygame.init()  # initialize pygame
 
 clock = pygame.time.Clock()
 
-screen = pygame.display.set_mode((1000, 800)) 
+screen = pygame.display.set_mode(SCREEN_SIZE)
 
-
-# Load the background image here. Make sure the file exists!
-
-bg = pygame.image.load(os.path.join("./", "background.png"))
-
-
-# ball = pygame.image.load(os.path.join("./", "ball.png"))
-
-ball = Entity("ball.png", (50,50), screen)
-
-# ball.velocity[0] = 1
-# ball.velocity[1] = 1
-
-
-pygame.mouse.set_visible(0)
-
-pygame.display.set_caption('Space Age Game')
-
-
-# fix indentation
-
+ball_1 = Entity("ball.png", (50,50), screen)
+ball_2 = Entity("ball.png", (50,50), screen)
+ball_3 = Entity("ball.png", (50,50), screen)
+ball_2.changePosition([100,0])
+ball_3.changePosition([300,0])
+bg = Game("background.png", screen)
 
 while True:
-
     clock.tick(60)
-
-    screen.blit(bg, (0, 0))
-
-    ball.update()
- 
+    bg.draw_background()
+    ball_1.update()
+    ball_2.update()
+    ball_3.update()
     for event in pygame.event.get():
-
+        if event.type == pygame.KEYDOWN:
+            if (event.key == pygame.K_x):
+                ball_2.jump()
+            if (event.key == pygame.K_z):
+                ball_1.jump()
         if event.type == pygame.QUIT:
-
             sys.exit()
 
 
